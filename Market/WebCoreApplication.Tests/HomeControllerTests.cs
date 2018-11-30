abab096a9cbc01a4b58ed2f8e8b50889327fe853
Nikolaich <kirill.0312@mail.ru>
2018-11-30 12:20:19 +0300

    ещё добавил пару тестов. второй с использованием InlineData для множественного прогона с разными данными

--- Market/WebCoreApplication.Tests/HomeControllerTests.cs
+++ Market/WebCoreApplication.Tests/HomeControllerTests.cs
@@ -30,16 +30,53 @@ namespace WebCoreApplication.Tests
         public void IndexActionModelIsCompletePricesUnder50()
         {
             // arrange
+
+            var fakeRepository = new ModelCompleteFakeRepository(p => p.Price < 50m);
+
             var controller = new Controllers.HomeController();
-            controller.Repository = new ModelCompleteFakeRepository(p=>p.Price <50m);
+            controller.Repository = fakeRepository;
 
             // act
             var viewResult = controller.Index() as ViewResult;
-            var model = viewResult?.ViewData.Model as IEnumerable<Product>;
+
+            var listFromModel = (viewResult?.ViewData.Model as IEnumerable<Product>)?.OrderBy(p => p.Name);
+            var list1 = fakeRepository.Products.OrderBy(p => p.Name);
 
             // assert
             var comparer = Comparer.Get<Product>(((product1, product2) => product1.Name == product2.Name && product1.Price == product2.Price));
-            Assert.Equal(new ModelCompleteFakeRepository(p => p.Price < 50m).Products.OrderBy(p => p.Name), model?.OrderBy(p => p.Name), comparer);
+            Assert.Equal(list1, listFromModel, comparer);
+        }
+
+        [Theory]
+        [InlineData(275, 48.95, 19.5, 24.95)]
+        [InlineData(5, 48.95, 19.5, 24.95)]
+        public void IndexActionManyModelIsComplete(decimal price1, decimal price2, decimal price3, decimal price4)
+        {
+            // arrange
+            var products = new List<Product>
+            {
+                new Product {Name = "P1", Price = price1},
+                new Product {Name = "P2", Price = price2},
+                new Product {Name = "P3", Price = price3},
+                new Product {Name = "P4", Price = price4},
+            };
+
+            var fakeRepository = new ModelCompleteFakeRepository(products);
+            
+            var controller = new Controllers.HomeController
+            {
+                Repository = fakeRepository
+            };
+
+
+            // act
+            var viewResult = controller.Index() as ViewResult;
+            var listFromModel = (viewResult?.ViewData.Model as IEnumerable<Product>)?.OrderBy(p => p.Name);
+            var list1 = fakeRepository.Products.OrderBy(p => p.Name);
+
+            // assert
+            var comparer = Comparer.Get<Product>(((product1, product2) => product1.Name == product2.Name && product1.Price == product2.Price));
+            Assert.Equal(list1, listFromModel, comparer);
         }
     }
 }
