279bb9dfbf3d8647037ee7b7fea089982452b9a8
knikolaich <kirill.0312@mail.ru>
2019-03-10 15:06:50 +0300

    Считаем ИМТ, выводов пока не делаем, только число

---
+++ Experiments/IMT/calcul.js
@@ -0,0 +1,23 @@
+function Person(weight, height, yearOfBirdth){
+this.weight = weight;
+this.height = height;
+
+var today= new Date();
+this.ageYear = today.getYear() +1900 - yearOfBirdth;  // Расчитываем по простому, нам тут не важна точность возраста для вычисления ИМТ
+this.Imt = weight * 10000 /(height*height);
+}	
+
+//расчитать ИМТ по росту/весу/возрасту
+function calculat(){
+	var weightE = document.getElementById("weight_id");
+	weight = weightE.value;
+	var heightE = document.getElementById("height_id");
+	height = heightE.value;
+	
+	var yearE = document.getElementById("year_id");
+	yearOfBirdth = yearE.value;
+	
+	
+	var person = new Person(weight, height, yearOfBirdth);
+	alert("возраст : "+person.ageYear + "\nИМТ: " + person.Imt);
+}
