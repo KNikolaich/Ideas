96d60143bde538fbbee2da946c666cc5b6d3ee6e
KNikolaich <kfedorov@kostasoft.spb.ru>
2019-11-18 01:16:24 +0300

    подключимся к БД посмотрим как она загружает даные

---
+++ ConsoleCore/DbConnector/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using DbConnector;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            foreach (var item in new ClassTest().GetEntryGenerator())
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
