8d4c7625fb634f56e8c1b3c922c98f833996eaae
kirill <kirill.nikolaich@yandex.ru>
2020-02-18 16:33:28 +0300

    Идеальный бот, но не работает на Core, не может подключить сертификат ssl

---
+++ SmartHome/WebCoreApp/WebCoreApp/Controllers/MessageController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Telegram.Bot.Types;
+using WebCoreApp.Models;
+
+namespace WebCoreApp.Controllers
+{
+    [Route("api/message/update")]
+    public class MessageController : Controller
+    {
+        // GET api/values
+//    ...
+
+    // POST api/values
+        [HttpPost]
+        public async Task<OkResult> Post([FromBody] Update update)
+        {
+            if (update == null) return Ok();
+
+            var commands = Bot.Commands;
+            var message = update.Message;
+            var botClient = await Bot.GetBotClientAsync();
+
+            foreach (var command in commands)
+            {
+                if (command.Contains(message))
+                {
+                    await command.Execute(message, botClient);
+                    break;
+                }
+            }
+            return Ok();
+        }
+    }
+}
---
+++ SmartHome/WebCoreApp/WebCoreApp/Models/Commands/Command.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace WebCoreApp.Models.Commands
+{
+    public abstract class Command
+    {
+        public abstract string Name { get; }
+
+        public abstract Task Execute(Message message, TelegramBotClient client);
+
+        public abstract bool Contains(Message message);
+    }
+}
