e9ef7c1d6449462828410163662b4bf75b0b1f74
kirill <kirill.nikolaich@yandex.ru>
2020-02-21 18:48:57 +0300

    Сделан новый сайт, на простом Asp.net

---
+++ SmartHome/BotWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BotWebApp.MvcApplication" Language="C#" %>
---
+++ SmartHome/BotWebApp/Models/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Configuration;
+
+namespace BotWebApp.Models
+{
+    public class AppSettings
+    {
+        private static AppSettings _single;
+
+        public AppSettings(string name, string token, string hookUrl)
+        {
+            Name = name;
+            Key = token;
+            Url = hookUrl;
+        }
+        
+        public string Url { get; }
+        public string Name { get; } 
+        public string Key { get; }
+
+        public static AppSettings Single
+        {
+            get
+            {
+                if (_single == null)
+                {
+                    var settings = WebConfigurationManager.AppSettings;
+                    _single = new AppSettings(settings["name"], settings["token"], settings["Url"]);
+                }
+                return _single;
+            }
+        }
+    }
+}
