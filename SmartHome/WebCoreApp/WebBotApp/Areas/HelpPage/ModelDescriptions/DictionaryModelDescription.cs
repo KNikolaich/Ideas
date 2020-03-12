e633c977cc68e0d877410e277c6461d85394506c
kirill <kirill.nikolaich@yandex.ru>
2020-03-12 11:19:14 +0300

    Прибиваем личные проекты из публичных репосов

--- SmartHome/WebCoreApp/WebBotApp/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WebBotApp.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
--- SmartHome/WebLoeskBot/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebLoeskBot.WebApiApplication" Language="C#" %>
