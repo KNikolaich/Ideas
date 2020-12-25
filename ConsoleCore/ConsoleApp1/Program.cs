818c5663d1cd8722d904baad404799632727665e
kirill <kirill.nikolaich@yandex.ru>
2020-12-25 17:14:27 +0300

    поднабралось тут

--- ConsoleCore/ConsoleApp1/Program.cs
+++ ConsoleCore/ConsoleApp1/Program.cs
@@ -1,16 +1,65 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Linq;
+using System.Xml.Serialization;
 
 namespace ConsoleApp1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            string helloWorld = "Hello World! Как делы? Будет ли мясо?";
-            var res = helloWorld.Split(" ").OrderBy(s => s).Aggregate("Begin :", (s, s1) => s + s1);
-            Console.WriteLine(res);
-            Console.ReadKey();
+            var xElement = CreateElement();
+            var riseAgainst = DeSerializer(xElement);
+            Console.WriteLine(string.Format("Band: {0}", riseAgainst.Name));
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine(string.Format("Album: {0}", riseAgainst.Albums.First().Name));
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("Song List:\r");
+            foreach (var s in riseAgainst.Albums.First().Songs)
+            {
+                Console.WriteLine(string.Format("Song: {0}", s.Name));
+            }
+            Console.ReadLine();
+        }
+
+
+        static XElement CreateElement()
+        {
+            return new XElement("Artist",
+                new XElement("Name", "Rise Against"),
+                new XElement("Albums",
+                    new XElement("Album",
+                        new XElement("Name", "Appeal to Reason"),
+                        new XElement("Songs",
+                            new XElement("Song", new XElement("Name", "Hero of War")),
+                            new XElement("Song", new XElement("Name", "Savior")))
+                    ))
+            );
+        }
+
+        static Artist DeSerializer(XElement element)
+        {
+            var serializer = new XmlSerializer(typeof(Artist));
+            return (Artist)serializer.Deserialize(element.CreateReader());
+        }
+
+        public class Artist
+        {
+            public string Name { get; set; }
+            public List<Album> Albums { get; set; }
+        }
+
+        public class Album
+        {
+            public string Name { get; set; }
+            public List<Song> Songs { get; set; }
+        }
+
+        public class Song
+        {
+            public string Name { get; set; }
         }
     }
 }
---
+++ irisoft/StaffTimes/StaffTimes/SubControls/TestForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Core.Model;
+using DevExpress.XtraEditors.Controls;
+
+namespace StaffTimes.SubControls
+{
+    public partial class TestForm : Form
+    {
+        private ContextAdapter _repository;
+
+        public TestForm()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            _repository = new ContextAdapter();
+            gridControl1.DataSource = _repository.GetDataTableTasks(2, DateTime.Today.AddYears(-1), DateTime.Today.AddDays(7));
+            _repositoryItemMRUEdit1.Items.AddRange(new object[] {
+                "www.devexpress.com",
+                "www.devexpress.com/ClientCenter/Downloads/#Trials",
+                "www.devexpress.com/ClientCenter/Purchase/"});
+            _repositoryItemMRUEdit1.Properties.TextEditStyle = TextEditStyles.DisableTextEditor;
+
+
+            base.OnLoad(e);
+        }
+    }
+}
